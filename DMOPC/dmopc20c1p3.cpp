9d9a98a567033508d8d9e664f63caabc3811e043
Steven Bai <51672429+stevenbai0724@users.noreply.github.com>
2021-01-04 12:23:30 -0500

    Add files via upload

--- DMOPC/dmopc20c1p3.cpp
+++ DMOPC/dmopc20c1p3.cpp
@@ -1,47 +1,45 @@
+//https://dmoj.ca/problem/dmopc20c1p3
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+#define int long long
+signed main(){
     cin.tie(nullptr)->sync_with_stdio(false);
 
     int n; cin>>n;
     vector<int>arr(n+1);
-    int mx = 0;
-    int mx2 = 0;   
-    int index = 0;
-    int count= 0;
-    for(int i =1; i<=n; i++){//input, finding largest
+    vector<int>s(n+1);
+    vector<int>p(n+1);
+    vector<int>psa(n+1);
+    vector<int>ssa(n+1);
+    int ans = (int)1e18;
+    for(int i =1; i<=n; i++){
         cin>>arr[i];
-        if(arr[i]>mx){
-            mx = max(mx, arr[i]);
-            index = i;
+    }
+    for(int i =2; i<n; i++){
+        if(arr[i]<arr[i-1]+p[i-1]){
+            p[i] = arr[i-1]+p[i-1]-arr[i];
         }
     }
-    int index2 = 0;
-    for(int i = index; i<=n; i++){
-        if(arr[i]>arr[i-1]) index2 = i;       
-    }
-    for(int i = index2; i<=n; i++){
-        mx2 = max(mx2, arr[i]);
-    }
-    bool dsc = true;
-    bool inc = true;
-    for(int i =1; i<=n; i++){
-        if(arr[i]<arr[i+1]) dsc = false;
-    }
-    for(int i =1; i<=n; i++){
-        if(arr[i]>arr[i+1]) inc = false;
-    }
-    if(dsc||inc) cout<<0;
-    else{
-        for(int i =index+1; i<=n; i++){
-            if(arr[i]<mx2) count += abs(arr[i]-mx2);
+    for(int i =n-1; i>=1; i--){
+        if(arr[i]<arr[i+1]+s[i+1]){
+            s[i] = arr[i+1]+s[i+1]-arr[i];
         }
-        cout<<count;
+    }
+    
+    int sum1 = 0;
+    int sum2 = 0;
+    for(int i =1; i<=n;i++){
+        sum1+= p[i];
+        psa[i] =sum1;
+    }
+    for(int i =n;i>=1; i--){
+        sum2+= s[i];
+        ssa[i] = sum2;
+    }
+    for(int i =1;i<n; i++){
+        ans = min(ans, psa[i] +ssa[i+1]);
     }
 
-
-
-
-
-    return 0 ;
+    cout<<ans;
+    return 0;
 }

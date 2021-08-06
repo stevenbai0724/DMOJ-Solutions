6693077deb66c772be86450de5ad643174e5e51f
stevenbai0724 <stevenbai0724@gmail.com>
2021-08-06 16:47:47 -0400

    8/6/21

---
+++ COCI/coci06c1p2.cpp
@@ -0,0 +1,18 @@
+//https://dmoj.ca/problem/coci06c1p2
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+#define int long long
+#define string std::string
+
+
+signed main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    
+    int r; cin>>r;
+    cout<<setprecision(500)<<fixed<<r*r*M_PI<<"\n"<<r*(2*r)<<"\n";
+
+
+    return 0;
+}
